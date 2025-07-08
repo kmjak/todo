f9dfa27f8dffc408423d1e873222e2aa760b8acd
kmjak <tymkoron1221@icloud.com>
2025-07-08 17:37:55 +0900

    feat: Todoを追加するTodoFormを追加した

---
+++ src/components/todo/TodoForm.tsx
@@ -0,0 +1,17 @@
+export default function TodoForm() {
+  return (
+    <form className="flex items-center gap-1 justify-center">
+      <input
+        type="text"
+        className="w-60 h-12 outline-none border-2 rounded-md text-xl px-2 py-1 focus:border-blue-400 transition-colors"
+        placeholder="Add a new task"
+      />
+      <button
+        type="submit"
+        className="w-20 h-12 border-2 rounded-md text-xl cursor-pointer hover:border-blue-400 active:border-blue-400 active:opacity-50 transition-colors duration-200"
+      >
+        Add
+      </button>
+    </form>
+  );
+}
