aaef941ecbf140b7d84b91bda259a82045343da3
kmjak <tymkoron1221@icloud.com>
2025-07-08 16:33:16 +0900

    feat: ページのHeaderを追加した

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="w-full h-16 bg-gray-800 text-white flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Todoアプリ</h1>
+    </header>
+  )
+}
