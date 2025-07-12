f7dc4053e49c23607e919f15219538917c8c4311
kmjak <tymkoron1221@icloud.com>
2025-07-13 05:21:43 +0900

    feat: todoのデータを格納するファイルを追加した

---
+++ src/config/todo/todos.ts
@@ -0,0 +1,7 @@
+import { Todo } from "@/types/todo/Todo";
+
+export const todos: Todo[] = [
+  { id: 1, name: "Todoアプリ作成", description: "Next.jsとTypeScriptで作成" },
+  { id: 2, name: "ランニング", description: "毎日5km走る" },
+  { id: 3, name: "ブロスタ", },
+];
