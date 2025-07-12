9f7468976b17f6edf968e1d17e36fc635381e1bd
kmjak <tymkoron1221@icloud.com>
2025-07-13 05:24:52 +0900

    feat: TodoListを使ってTodoを一覧で表示するようにした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import TodoForm from "@/components/todo/TodoForm";
+import TodoList from "@/components/todo/TodoList";
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center py-4">
+    <main className="flex flex-col justify-center items-center py-4">
       <TodoForm />
+      <TodoList />
     </main>
   );
 }
