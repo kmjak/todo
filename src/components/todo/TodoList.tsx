e8f5a509bc15f00e7494985d243aaf1946eddeaa
kmjak <tymkoron1221@icloud.com>
2025-07-13 05:21:43 +0900

    refactor: TodoItemの表示コンポーネントとtodosをimportして使うように修正した

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -1,22 +1,12 @@
+import TodoItem from "@/components/todo/TodoItem";
+import { todos } from "@/config/todo/todos";
 import { Todo } from "@/types/todo/Todo";
 
 export default function TodoList() {
-  const todos: Todo[] = [
-    { id: 1, name: "Todoアプリ作成", description: "Next.jsとTypeScriptで作成" },
-    { id: 2, name: "ランニング", description: "毎日5km走る" },
-    { id: 3, name: "ブロスタ", },
-  ];
-
   return (
     <ul className="flex flex-col gap-2 py-4">
       {todos.map((todo:Todo) => (
-        <div className="flex items-center justify-between gap-2 cursor-pointer px-3 py-2 text-xl w-80 border border-black rounded-lg" key={todo.id}>
-          <p className="">{todo.name}</p>
-          <div className="flex items-center gap-2">
-            <p className="text-green-500 hover:opacity-40 transition-opacity">更新</p>
-            <p className="text-red-500 hover:opacity-40 transition-opacity">削除</p>
-          </div>
-        </div>
+        <TodoItem key={todo.id} name={todo.name} />
       ))}
     </ul>
   );
