be22ce963cba803422451e7da8aa0a5a8ca81937
kmjak <tymkoron1221@icloud.com>
2025-07-14 03:59:58 +0900

    refactor: todosをtodoItemsに修正した

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -1,11 +1,11 @@
 import TodoItem from "@/components/todo/TodoItem";
-import { todos } from "@/config/todo/todos";
+import { todoItems } from "@/config/todo/todos";
 import { Todo } from "@/types/todo/Todo";
 
 export default function TodoList() {
   return (
     <ul className="flex flex-col gap-2 py-4">
-      {todos.map((todo:Todo) => (
+      {todoItems.map((todo:Todo) => (
         <TodoItem key={todo.id} name={todo.name} id={todo.id} />
       ))}
     </ul>
