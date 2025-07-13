d6545bbf0114391141a28bd6d926278db748db24
kmjak <tymkoron1221@icloud.com>
2025-07-14 04:34:38 +0900

    feat: useTodoを使用してtodoを追加できる機能を追加した

--- src/components/todo/TodoForm.tsx
+++ src/components/todo/TodoForm.tsx
@@ -1,10 +1,17 @@
+"use client"
+
+import useTodo from "@/hooks/todo/useTodo";
+
 export default function TodoForm() {
+  const { handleAddTodo, handleChangeNewTodoName, newTodoName } = useTodo();
   return (
-    <form className="flex items-center gap-1 justify-center">
+    <form className="flex items-center gap-1 justify-center" onSubmit={handleAddTodo}>
       <input
         type="text"
         className="w-60 h-12 outline-none border-2 rounded-md text-xl px-2 py-1 focus:border-blue-400 transition-colors"
         placeholder="Add a new task"
+        value={newTodoName}
+        onChange={handleChangeNewTodoName}
       />
       <button
         type="submit"
